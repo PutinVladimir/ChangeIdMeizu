b594729e075e5432b3577133111d108fa2e38784
Putin Vladimir <VladimirPutinGit@site.tm>
2018-01-06 12:02:01 +0700

    M5 Note, M5s, M5, MX6, U10, U20 added

--- java/ru/meizu/changeid/MainActivity.java
+++ java/ru/meizu/changeid/MainActivity.java
@@ -54,6 +54,11 @@ public class MainActivity extends Activity {
        sudo("echo -e '5' | dd of=/dev/block/mmcblk0 bs=1 seek=13107717 count=1");
        break;
 
+      case "M5 Note":
+       sudo("dd if=/dev/block/mmcblk0p28 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
+       sudo("echo -e '5' | dd of=/dev/block/mmcblk0p28 bs=1 seek=515 count=1");
+       break;
+
       case "M6 Note":
        sudo("dd if=/dev/block/mmcblk0p50 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
        sudo("echo -e '5' | dd of=/dev/block/mmcblk0p50 bs=1 seek=515 count=1");
@@ -72,10 +77,36 @@ public class MainActivity extends Activity {
        sudo("echo -e '1' | dd of=/dev/block/mmcblk0 bs=1 seek=568885769 count=1");
        break;
 
+      case "M5s":
+       sudo("dd if=/dev/block/mmcblk0p8 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
+       sudo("echo -e '5' | dd of=/dev/block/mmcblk0p18 bs=1 seek=517 count=1");
+       break;
+
+      case "M5":
+       sudo("dd if=/dev/block/mmcblk0p8 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
+       sudo("echo -e '5' | dd of=/dev/block/mmcblk0p15 bs=1 seek=517 count=1");
+       sudo("echo -e '2' | dd of=/dev/block/mmcblk0p15 bs=1 seek=521 count=1");
+       break;
+
       case "M6":
        sudo("dd if=/dev/block/mmcblk0p8 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
        sudo("echo -e '5' | dd of=/dev/block/mmcblk0p8 bs=1 seek=515 count=1");
        sudo("echo -e '2' | dd of=/dev/block/mmcblk0p8 bs=1 seek=519 count=1");
+       break;
+
+      case "MX6":
+       sudo("dd if=/dev/block/mmcblk0p16 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
+       sudo("echo -e '5' | dd of=/dev/block/mmcblk0p16 bs=1 seek=517 count=1");
+       break;
+
+      case "U10":
+       sudo("dd if=/dev/block/mmcblk0p15 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
+       sudo("echo -e '5' | dd of=/dev/block/mmcblk0p15 bs=1 seek=517 count=1");
+       break;
+
+      case "U20":
+       sudo("dd if=/dev/block/mmcblk0p28 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
+       sudo("echo -e '5' | dd of=/dev/block/mmcblk0p28 bs=1 seek=517 count=1");
        break;
 
       default:
