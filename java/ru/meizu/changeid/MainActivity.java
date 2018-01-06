97bec521a38f0d241bd8864bac90a4b27c1bec01
Putin Vladimir <VladimirPutinGit@site.tm>
2018-01-06 13:43:11 +0700

    PRO 5 added

--- java/ru/meizu/changeid/MainActivity.java
+++ java/ru/meizu/changeid/MainActivity.java
@@ -65,6 +65,11 @@ public class MainActivity extends Activity {
        sudo("echo -e '2' | dd of=/dev/block/mmcblk0p50 bs=1 seek=519 count=1");
        break;
 
+      case "PRO 5":
+       sudo("dd if=/dev/block/platform/15570000.ufs/by-name/proinfo of=/storage/emulated/0/devinfo_backup_$(date +%Y-%m-%d).img");
+       sudo("echo -e 'intl_official' | dd of=/dev/block/platform/15570000.ufs/by-name/proinfo bs=1 seek=44 count=13");
+       break;
+
       case "PRO 6":
        sudo("dd if=/dev/block/mmcblk0p4 of=/sdcard/devinfo_backup_$(date +%Y-%m-%d).img");
        sudo("echo -e '5' | dd of=/dev/block/mmcblk0 bs=1 seek=568885765 count=1");
